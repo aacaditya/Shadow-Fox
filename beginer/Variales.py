54ba86298b5cae59f52087ef96d91521e7b63bd4
aacaditya <adityachaudhari9102004@gmail.com>
2025-03-19 23:32:23 +0530

    Update Variales.py

--- beginer/Variales.py
+++ beginer/Variales.py
@@ -1,3 +1,18 @@
 # 1. create a variable named pi and store the value 22/7 in it.Now check the data type of this variable.
 pi=22/7
 print(type(pi))
+
+# 2. Create a variable called for and assign it a value 4. See what happens and find out the reason behind the behavior that you see. 
+for=4
+print(for)
+# if i create variable for and assign value 4 and then print then i will got syntax error:invalid syntax
+
+
+# 3. Store the principal amount, rate of interest, and time in different variables and then calculate the Simple Interest for 3years. Formula: Simple Interest = P x R x T / 100
+P = 10000 #principle interest
+R = 5     #Rate of interest
+T = 3     #Time (in year)
+
+SI=(P * R * T) / 100  #simple interest
+
+print("Simple Interest:",SI)
